245f1ef4a7a2a729e2fa18b590d3f4eae87be526
hiroka <mitsunagahrk87@gmail.com>
2024-02-18 10:31:15 +0900

    section11

--- section11/src/App.tsx
+++ section11/src/App.tsx
@@ -1,3 +1,27 @@
+import { UserCard } from "./components/UserCard";
+import { useAllUsers } from "./hooks/useAllUsers";
+
 export default function App() {
-  return <div className="App"></div>;
+  const { getUsers, userProfiles, loading, error } = useAllUsers();
+
+  const onClickFetchUser = () => {
+    getUsers();
+  };
+  return (
+    <div className="App">
+      <button onClick={onClickFetchUser}>データ取得</button>
+      <br />
+      {error ? (
+        <p>データの取得に失敗しました</p>
+      ) : loading ? (
+        <p>データをローディングしています</p>
+      ) : (
+        <>
+          {userProfiles.map((user) => (
+            <UserCard key={user.id} user={user} />
+          ))}
+        </>
+      )}
+    </div>
+  );
 }
